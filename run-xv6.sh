21357cb6c96054933bd76a10519798766f36f34a
v15hv4 <vishva2912@gmail.com>
2021-10-27 08:05:54 +0530

    Add formatting rules

--- run-xv6.sh
+++ run-xv6.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 export RDIR=/home/user/xv6
-docker run --rm -v $(realpath .):$RDIR -w $RDIR -it $(docker build -q .) make qemu "$@"
+docker run --rm -v $(realpath .):$RDIR -w $RDIR -it $(docker build -q .) "$@"
